e5d2a5594d0219e4bf09baccf314366833c03e95
Naresh Patchipulusu <aswini.naresh007@gmail.com>
2015-11-19 18:26:53 +0530

    Next Commit by naresh with hibernate libraries on 19112015

--- src/gitdemoprogect/HibernateUtil.java
+++ src/gitdemoprogect/HibernateUtil.java
@@ -24,6 +24,7 @@ public class HibernateUtil {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
             sessionFactory = new AnnotationConfiguration().configure("/resources/hibernate.cfg.xml").buildSessionFactory();
+            
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
