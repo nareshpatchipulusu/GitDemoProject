f8263ade9d24865f92396e43e9bea630bafe74c2
Naresh Patchipulusu <aswini.naresh007@gmail.com>
2015-11-19 17:09:41 +0530

    Next Commit by naresh with hibernate on 19112015

---
+++ src/gitdemoprogect/Student.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemoprogect;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author nareshp
+ */
+public class Student implements Serializable {
+    
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+
+    public  Student(){
+        
+    }
+    
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    
+}
